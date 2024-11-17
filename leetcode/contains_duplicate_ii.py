e8108e438fbb03998bca7e8d1329c60dc6d87465
Matthew Mach <matthew.mach@protonmail.com>
2024-11-17 13:22:17 -0500

    Solved contains duplicate ii

---
+++ leetcode/contains_duplicate_ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        dic = {}
+        for i in range(len(nums)):
+            num = nums[i]
+            if num in dic:
+                if i - dic[num] <= k:
+                    return True
+            dic[num] = i
+        return False
